8f1f6d9802d69e89886b14cae94683e1c72fd94f
jsw7027 <jsw7027@gmail.com>
2021-02-17 14:14:43 +0900

    flatList added

--- pages/SelectPage.js
+++ pages/SelectPage.js
@@ -11,6 +11,47 @@ export default function SelectPage({navigation}) {
   }
 
 
+  const ItemSeparatorView = () => {
+    return (
+      //Item Separator
+      <View
+      style={{ height: 0.5, width: '100%', backgroundColor: '#C8C8C8' }}
+    />
+    );
+  };
+
+  
+
+  const ItemView = ({ item }) => {
+
+    return (
+      // Single Comes here which will be repeatative for the FlatListItems
+      <TouchableOpacity style={styles.chat} onPress={goToChat}>
+            <View style={styles.chat1}><Text style={styles.chatName}>{item.title}</Text><Text style={styles.chatPeople}>6</Text></View>
+            <View style={styles.chat2}>
+              <View style={styles.chatImage}>
+                <Image style={styles.jpg}
+                       source={{uri: item.image}}></Image>
+              </View>
+              <View style={styles.chatContnet}>
+                <Text style={styles.hostName}>{item.host}</Text>
+                <Text style={styles.hostIntro}
+                      numberOfLines={1}>{item.subTitle}</Text>
+              </View>
+              <View style={styles.chatDate}><Text style={styles.date}>{item.date}</Text></View>
+            </View>
+          </TouchableOpacity>
+    );
+  };
+
+
+  const selectList = [
+    {title: "React-native Study", host:"안승우", subTitle:"expo 이용해서 앱출시까지 마쳐봐요", date:"2021-02-09"+ " 개설", image:"https://firebasestorage.googleapis.com/v0/b/studyapp-3e58f.appspot.com/o/profile.jpg?alt=media&token=dc164977-d60c-4ae6-a6a3-46062c73b7e4"},
+    {title: "스타트업 스터디", host:"정승완", subTitle:"창업 컨퍼런스 인원 모집합니다!", date:"2021-02-09"+ " 개설", image:"https://firebasestorage.googleapis.com/v0/b/studyapp-3e58f.appspot.com/o/profile2.jpg?alt=media&token=be4087a4-4ff5-4fe7-b0a2-b1c233f99313"},
+    {title: "JAVA 스터디", host:"이유석", subTitle:"코딩테스트 공부 같이해요", date:"2021-02-09"+ " 개설",image:"https://firebasestorage.googleapis.com/v0/b/studyapp-3e58f.appspot.com/o/profile3.jpg?alt=media&token=d83432d6-bc4d-4df3-96a3-966b62653934"},
+    {title: "애견 스터디", host:"Rex", subTitle:"나를 범해주세요 주인님 하앍", date:"2021-02-09"+ " 개설", image:"https://firebasestorage.googleapis.com/v0/b/studyapp-3e58f.appspot.com/o/profile4.jpg?alt=media&token=78f0a31b-2b53-4a52-aaa7-7b66a4a7dc4c"},
+  ]
+
   return (
       <View style={styles.container}>
         
@@ -21,76 +62,12 @@ export default function SelectPage({navigation}) {
         <ScrollView style={styles.chatContainer}>
 
 
-          <TouchableOpacity style={styles.chat} onPress={goToChat}>
-            <View style={styles.chat1}><Text style={styles.chatName}>React-native 스터디</Text><Text style={styles.chatPeople}>6</Text></View>
-            <View style={styles.chat2}>
-              <View style={styles.chatImage}>
-                <Image style={styles.jpg}
-                       source={require('../assets/profile.jpg')}></Image>
-              </View>
-              <View style={styles.chatContnet}>
-                <Text style={styles.hostName}>안승우</Text>
-                <Text style={styles.hostIntro}
-                      numberOfLines={1}>expo 이용해서 앱출시까지 마쳐봐요!</Text>
-              </View>
-              <View style={styles.chatDate}><Text style={styles.date}>2021-02-09 개설</Text></View>
-            </View>
-          </TouchableOpacity>
-
-
-
-
-
-          <TouchableOpacity style={styles.chat}>
-            <View style={styles.chat1}><Text style={styles.chatName}>스타트업 스터디</Text><Text style={styles.chatPeople}>8</Text></View>
-            <View style={styles.chat2}>
-              <View style={styles.chatImage}>
-                <Image style={styles.jpg}
-                       source={require('../assets/profile2.jpg')}></Image>
-              </View>
-              <View style={styles.chatContnet}>
-                <Text style={styles.hostName}>정승완</Text>
-                <Text style={styles.hostIntro}
-                      numberOfLines={1}>창업 컨퍼런스 인원 모집합니다!</Text>
-              </View>
-              <View style={styles.chatDate}><Text style={styles.date}>2021-02-09 개설</Text></View>
-            </View>
-          </TouchableOpacity>
-
-
-          <TouchableOpacity style={styles.chat}>
-            <View style={styles.chat1}><Text style={styles.chatName}>JAVA 스터디</Text><Text style={styles.chatPeople}>10</Text></View>
-            <View style={styles.chat2}>
-              <View style={styles.chatImage}>
-                <Image style={styles.jpg}
-                       source={require('../assets/profile3.jpg')}></Image>
-              </View>
-              <View style={styles.chatContnet}>
-                <Text style={styles.hostName}>이유석</Text>
-                <Text style={styles.hostIntro}
-                      numberOfLines={1}>코딩테스트 공부 같이해요!</Text>
-              </View>
-              <View style={styles.chatDate}><Text style={styles.date}>2021-02-09 개설</Text></View>
-            </View>
-          </TouchableOpacity>
-
-
-          <TouchableOpacity style={styles.chat}>
-            <View style={styles.chat1}><Text style={styles.chatName}>산책 스터디</Text><Text style={styles.chatPeople}>6</Text></View>
-            <View style={styles.chat2}>
-              <View style={styles.chatImage}>
-                <Image style={styles.jpg}
-                       source={require('../assets/profile4.jpg')}></Image>
-              </View>
-              <View style={styles.chatContnet}>
-                <Text style={styles.hostName}>Rex</Text>
-                <Text style={styles.hostIntro}
-                      numberOfLines={1}>렉스 귀여워</Text>
-              </View>
-              <View style={styles.chatDate}><Text style={styles.date}>2021-02-09 개설</Text></View>
-            </View>
-          </TouchableOpacity>
-
+          <FlatList
+            data={selectList}
+            ItemSeparatorComponent={ItemSeparatorView}
+            renderItem={ItemView}
+            keyExtractor={(item, index) => index.toString()}
+            />
         </ScrollView>
         <View style={styles.buttonContainer}>
           <TouchableOpacity style={styles.underButton}><Image style={styles.buttonImage} source={require('../assets/homeButton.png')}></Image></TouchableOpacity>
