09062bf3ec4335035ab7e0301a0360271852c983
rio319-an <rio319@naver.com>
2021-06-25 16:51:55 +0900

    test

--- pages/tutorApplyPage.js
+++ pages/tutorApplyPage.js
@@ -1,7 +1,85 @@
 import React, { useEffect, useState, } from 'react';
 import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView,FlatList,TextInput,TouchableWithoutFeedback} from 'react-native';
+import firebase from 'firebase'
+import * as ImagePicker from 'expo-image-picker';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAhALJl-3lVlNXoIueqpfcR1gfLEkJXOxc",
+    authDomain: "studyapp-3e58f.firebaseapp.com",
+    databaseURL: "https://studyapp-3e58f-default-rtdb.firebaseio.com",
+    projectId: "studyapp-3e58f",
+    storageBucket: "studyapp-3e58f.appspot.com",
+    messagingSenderId: "514395246608",
+    appId: "1:514395246608:web:2c39981eed6d3602b4fa95",
+    measurementId: "G-GL08SPFWYS"
+  };
+  
+  
+  if(firebase.apps.length===0){
+    firebase.initializeApp(firebaseConfig);
+  }
 
 export default function tutorApplyPage({navigation}){
+
+    let openImagePickerAsync = async () => {
+        let permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    
+        if (permissionResult.granted === false) {
+          alert("Permission to access camera roll is required!");
+          return;
+        }
+    
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        console.log(pickerResult);
+      }
+
+      
+
+      
+
+      
+
+    console.disableYellowBox = true;
+    const [tutorName, setTutorName] = useState('');
+    const [age, setAge] = useState('');
+    const [school, setSchool] = useState('');
+    const [course, setCourse] = useState('');
+    const [tutorIntro, setTutorIntro] = useState('');
+    const [tutorSubject, setTutorSubject] = useState('');
+    const [classExplain, setClassExplain] = useState('');
+    const [classHow, setClassHow] = useState('');
+    const [classTime, setClassTime] = useState('');
+    const [appeal, setAppeal] = useState('');
+    const [selectedImage, setSelectedImage] = React.useState(null);
+
+    
+
+    function Request(tutorName,age,school,course,tutorIntro,tutorSubject,classExplain,classHow,classTime,appeal){
+        const inputlist = [tutorName,age,school,course,tutorIntro,tutorSubject,classExplain,classHow,classTime,appeal]
+        const ref = firebase.database().ref().child('mentoList');
+        
+        if (inputlist.includes('')){
+            alert('모든 항목을 입력해주세요!');
+        } else{
+            ref.push().set({
+            'mentoApplyList':{
+                tutorName :tutorName,
+                age :age,
+                school: school,
+                course: course,
+                tutorIntro :tutorIntro,
+                tutorSubject :tutorSubject,
+                classExplain :classExplain,
+                classHow :classHow,
+                classTime :classTime,
+                appeal :appeal
+                }
+                })
+            
+        navigation.navigate("MainPage")
+            }
+    }
+
  return(
     <View style={styles.container}>
          <ScrollView showsVerticalScrollIndicator={false}>
@@ -14,25 +92,33 @@ export default function tutorApplyPage({navigation}){
                 <TextInput 
                 style={styles.input}
                 placeholder="이름"
+                onChangeText={text => setTutorName(text)}
+                value={tutorName}
                 />
                 <Text style={styles.title}>나 이</Text>
                 <TextInput 
                 style={styles.input}
                 placeholder="나이"
+                onChangeText={text => setAge(text)}
+                value={age}
                 />
                 <Text style={styles.title}>학 교</Text>
                 <TextInput 
                 style={styles.input}
                 placeholder="학교명을 입력하세요"
+                onChangeText={text => setSchool(text)}
+                value={school}
                 />
                 <Text style={styles.title}>전 공</Text>
                 <TextInput 
                 style={styles.input}
                 placeholder="전공명을 입력하세요"
+                onChangeText={text => setCourse(text)}
+                value={course}
                 />
                 <Text style={styles.title}>사 진</Text>
                 <View style={styles.imageBox}>
-                    <TouchableOpacity style={styles.imageInput}>
+                    <TouchableOpacity style={styles.imageInput} onPress={openImagePickerAsync}>
                         <Text style={{fontSize:24, color:'gray'}}>+</Text>
                     </TouchableOpacity>   
                 </View>
@@ -45,35 +131,47 @@ export default function tutorApplyPage({navigation}){
                     <TextInput 
                     style={styles.input}
                     placeholder="이름"
+                    onChangeText={text => setTutorIntro(text)}
+                    value={tutorIntro}
                     />
                 <Text style={styles.title}>과 목</Text>
                 <TextInput 
                 style={styles.input}
                 placeholder="이름"
+                onChangeText={text => setTutorSubject(text)}
+                value={tutorSubject}
                 />
                 <Text style={styles.title}>과목별 수업내용</Text>
                 <TextInput 
                 style={styles.input2} 
                 placeholder="과목별 수업내용을 구체적으로 적어주세요!" 
                 multiline={true}
+                onChangeText={text => setClassExplain(text)}
+                value={classExplain}
                 />
                 <Text style={styles.title}>수업 방식</Text>
                 <TextInput 
                 style={styles.input2} 
                 placeholder="수업방식을 구체적으로 적어주세요!" 
                 multiline={true}
+                onChangeText={text => setClassHow(text)}
+                value={classHow}
                 />
                 <Text style={styles.title}>가능 시간</Text>
                 <TextInput 
                 style={styles.input1} 
                 placeholder="가능한 시간을 입력하세요" 
                 multiline={true}
+                onChangeText={text => setClassTime(text)}
+                value={classTime}
                 />
                 <Text style={styles.title}>어 필</Text>
                 <TextInput 
                 style={styles.input1} 
                 placeholder="어필할 내용을 적어주세요!" 
                 multiline={true}
+                onChangeText={text => setAppeal(text)}
+                value={appeal}
                 />
             <View style={styles.infoBox}>
                 <Image style={{width:6,height:6,marginTop:29,marginLeft:5}}
@@ -86,7 +184,8 @@ export default function tutorApplyPage({navigation}){
                 </TouchableOpacity>
             </View>
             <TouchableOpacity 
-            style={styles.button}>
+            style={styles.button}
+            onPress={()=>Request(tutorName,age,school,course,tutorIntro,tutorSubject,classExplain,classHow,classTime,appeal)}>
                 <Text style={{color:'#fff', fontWeight:'500', fontSize:14}}>신 청 하 기</Text>
             </TouchableOpacity>
          </ScrollView>
